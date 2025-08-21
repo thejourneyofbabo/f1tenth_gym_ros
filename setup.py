2c73ee0d629c03291fb399186b11c45e77ccbdca
JisangYun <journeyofbabo@gmail.com>
2025-08-21 12:23:27 +0900

    update new map

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         (os.path.join('share', package_name, 'launch'), glob('launch/*.xacro')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.rviz')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
